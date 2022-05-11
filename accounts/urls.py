53b6df4adc0d4018f5bdd288ac5e816569c719b6
YeasirArafatRatul <yeasir1694@diit.info>
2022-05-11 19:49:21 +0600

    first commit

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
