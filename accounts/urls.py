5d40dec7731b143e951ddba83cdd774fe563a588
YeasirArafatRatul <yeasir1694@diit.info>
2022-05-18 23:44:02 +0600

    login, logout, accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView\
+
 
 app_name = 'accounts'
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', views.registration, name='register'),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,25 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
+from django.contrib.messages import success, error
+from . forms import CustomUserCreationForm
+
 
 # Create your views here.
 
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+
+def registration(request):
+    if request.method == 'POST':  
+        form = CustomUserCreationForm(request.POST) 
+        print("I am here")
+        if form.is_valid():  
+            form.save()
+            success(request, 'Account created successfully')  
+    else:  
+        form = CustomUserCreationForm()
+    context = {  
+        'form':form  
+    }   
+    return render(request, 'registration/registration.html',context)
